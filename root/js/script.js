ab5b61922004fdbd286d6395b65dc19b7e0e7425
Jorge E Sosa <jorgeduardo.1994@gmail.com>
2018-05-16 17:57:57 -0500

    services done, added google map

--- root/js/script.js
+++ root/js/script.js
@@ -2,4 +2,19 @@
 // 	$(".leonardo").css("display", "block");
 // });
 
+function initMap(){
+	var location = {lat: 29.785785, lng: -95.824396};
+	var center = {lat: 29.761331, lng: -95.358841};
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 10,
+		center: center
+	});
+
+	var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      title: 'Three Balloons'
+    });
+
+}
 
